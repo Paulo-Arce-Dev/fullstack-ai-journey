1a03494c0529e3498adf4940f84c4dcf54eccbea
Paulo Arce <codebypaulo@gmail.com>
2025-08-28 04:49:19 -0300

    resolviendo ejercicios

---
+++ 01_fundamentos/python/ejercicios-crash-course/variables/ejercicio_01.py
@@ -0,0 +1,15 @@
+"""
+Escriba un programa separado para realizar cada uno de estos ejercicios. Guarde cada programa con un nombre de archivo que siga las convenciones estándar de Python, usando
+letras minúsculas y guiones bajos, como simple_message.py y simple_messages.py.
+
+2-1. Mensaje simple: asigne un mensaje a una variable y luego imprímalo.
+2-2. Mensajes simples: asigne un mensaje a una variable e imprímalo. Luego cambie el valor de la variable a un nuevo mensaje e imprima el nuevo mensaje.
+"""
+
+# 2-1
+frase_motivacional = 'La paciencia es el algoritmo del éxito'
+print(frase_motivacional)
+
+# 2-2
+frase_motivacional_2 = 'En el silencio nace la mejor lógica'
+print(frase_motivacional_2)
