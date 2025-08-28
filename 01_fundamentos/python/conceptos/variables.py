14d8fb6c535621e254672402f04da58780eae359
Paulo Arce <codebypaulo@gmail.com>
2025-08-28 04:37:13 -0300

    aprendiendo variables en python

---
+++ 01_fundamentos/python/conceptos/variables.py
@@ -0,0 +1,17 @@
+# Variables en Python
+
+# Declarando una variable en una línea
+mensaje = "Hola mundo desde Python Crash Course";
+pais = 'Argentina';
+age = 24;
+esta_casado = False;
+stack_tecnologico = ['HTML', 'CSS', 'JS', 'React', 'Python', 'SQL', 'Flask', 'PostgreSQL', 'Numbpy', 'Pandas'];
+info_personal = {
+    'nombre':'Paulo',  
+    'pais':'Argentina',
+    'edad': 24,
+    'hobbies': ["Tomar mate 🧉", "Leer 📕","Jugar videojuegos 🎮", "Escuchar música 🎵"]
+}
+
+# Declarando una variable en múltiples líneas
+apellido, provincia,  = 'Arce', 'Córdoba';
