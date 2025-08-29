31fe7c6c0bcda40d2605fb146e73db9969240316
Paulo Arce <codebypaulo@gmail.com>
2025-08-29 20:55:40 -0300

    ejercicios de numbers

---
+++ 01_fundamentos/python/ejercicios-crash-course/number/ejercicios_01.py
@@ -0,0 +1,25 @@
+# Ejercicios con el tipo de datos Number
+
+"""  
+2-9. Número ocho: Escriba operaciones de suma, resta, división y multiplicación que den como resultado el número 8. Asegurarse de encerrar sus operaciones en llamadas print() para ver los resultados. Deberías crear cuatro líneas que se vean asi: 
+    print(5 + 3)
+
+2-10. Número favorito: Utilice una variable para representar su número favorito. Luego, usando esa variable, crea un mensaje que revele tu número favorito. Imprime ese mensaje
+"""
+
+# 2-9
+suma = 1 + 7
+resta = 10 - 2
+division = int(40 / 5)
+multiplicacion = (1 + 3) * 2
+
+print(suma)
+print(resta)
+print(division)
+print(multiplicacion)
+
+
+# 2-10
+numero_favorito = 18
+mensaje = f"Mi número favorito es: {numero_favorito}"
+print(mensaje)
