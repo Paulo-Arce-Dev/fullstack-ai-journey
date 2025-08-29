66572f21940410ce9837b23d44ddfb94880364aa
Paulo Arce <codebypaulo@gmail.com>
2025-08-29 20:45:19 -0300

    aprendiendo el tipo de dato number

---
+++ 01_fundamentos/python/conceptos/number.py
@@ -0,0 +1,33 @@
+# Number
+
+# Integer (enteros)
+edad = 24
+año_actual = 2025
+
+# operaciones básicas con Integer
+suma_enteros = 2 + 5
+resta_enteros = 12 - 5
+division_enteros = 42 / 5
+multiplicacion_enteros = 2 * 5
+
+# Float (flotantes)
+altura_en_metros = 1.80
+
+# operaciones básicas con Float
+suma_flotantes = 2.45 + 5.8
+resta_flotantes = 12.5 - 5.1
+division_flotantes = 42.5 / 8.5
+multiplicacion_flotantes = 2.67 * 5.22
+
+# combinacion entre Integer y Float
+altura_en_centimetros = 180
+crecimiento_altura_centimetros = 1.5
+
+altura_actual = altura_en_centimetros + crecimiento_altura_centimetros
+print(f"La altura actual es de: {altura_actual}")
+
+# Guiones bajos para núumeros grandes 
+
+# funciona tanto para Integer y Float
+edad_universo = 14_000_000_000
+print(edad_universo) # 14000000000
