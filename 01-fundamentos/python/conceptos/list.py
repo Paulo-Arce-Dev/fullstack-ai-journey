40f284d062c0d6fb6a2d6e4f4c9fcf11bb772881
Paulo Arce <codebypaulo@gmail.com>
2025-09-01 16:18:39 -0300

    aprendiendo listas

--- 01-fundamentos/python/conceptos/list.py
+++ 01-fundamentos/python/conceptos/list.py
@@ -1,1 +1,16 @@
-# Listas
+# Listas []
+
+bicicletas = ['trek', 'redline', 'cannondale']
+print(bicicletas)
+
+# acceder a elementos de lisas
+print(bicicletas[0])
+print(bicicletas[0].title())
+print(bicicletas[1])
+print(bicicletas[2])
+
+# último elemento de una lista
+print(bicicletas[-1])
+
+mensaje = f"Mi primera bicicleta fue una {bicicletas[0].title()}"
+print(mensaje)
