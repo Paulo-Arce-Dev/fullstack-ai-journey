3253644279d31b00f699f8a1d3834ff18e53012a
Paulo Arce <codebypaulo@gmail.com>
2025-09-09 22:16:43 -0300

    cajero automático básico por consola

---
+++ 01-fundamentos/javascript-vanilla/practica-basica/bucles/cajero-automatico/app.js
@@ -0,0 +1,20 @@
+// Cajero automático
+
+// pedir al usuario: monto de dinero a gastar
+// pedir al usuario: cuanto desea retirar
+let montoTotal = parseFloat(
+  prompt("Ingrese el monto total de su dinero en su cuenta")
+);
+console.log(`Saldo total de la cuenta: ${montoTotal}`);
+
+let montoDescontar = parseFloat(prompt("Ingrese cuanto quiere gastar"));
+console.log(`Gasto del usuario a descontar: ${montoDescontar}`);
+
+while (montoTotal >= montoDescontar) {
+  montoTotal = montoTotal - montoDescontar;
+  console.log(`Su saldo actual es: ${montoTotal}`);
+
+  montoDescontar = parseFloat(prompt("Ingrese cuanto quiere gastar"));
+  console.log(`Gasto del usuario: ${montoDescontar}`);
+}
+console.log("Saldo insuficiente.");
