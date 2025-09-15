4991d50574f4892afca46f25781470d26764b91e
Paulo Arce <codebypaulo@gmail.com>
2025-09-15 19:37:25 -0300

    agregando comentarios al programa

--- 01-fundamentos/javascript-vanilla/practica-basica/arreglos-y-cadenas/vocales-en-palabras/app.js
+++ 01-fundamentos/javascript-vanilla/practica-basica/arreglos-y-cadenas/vocales-en-palabras/app.js
@@ -4,11 +4,14 @@ let palabraUsusario = prompt(
   "Ingrese una palabra para saber cuántas vocales tiene"
 );
 
+// funcion para contar las vocales
 const contarVocales = (palabra) => {
   const vocales = "aáeéiíoóuú";
   let cantidadVocales = 0;
 
+  // recorrer cada letra de la palabra
   for (const letra of palabra) {
+    // buscamos coincidencia de vocales y la palabra
     if (vocales.includes(letra.toLowerCase())) {
       cantidadVocales++;
     }
@@ -16,4 +19,5 @@ const contarVocales = (palabra) => {
   return `La palabra ${palabraUsusario} tiene ${cantidadVocales} vocales`;
 };
 
+// mostrando resultado en consola
 console.log(contarVocales(palabraUsusario));
